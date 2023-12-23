8f03b9c5af9b6cc068950db5d347f10b5a8d96be
m03azy <iammosesfaustin@gmail.com>
2023-12-23 12:13:23 +0300

    some modification have been done

--- admin.php
+++ admin.php
@@ -11,17 +11,17 @@
     $adminUsername = $_SESSION['admin_user'];
     // $filename = 'new_image.jpg';
 
-    $query = "UPDATE Admin SET image = $filename WHERE Auname = $adminUsername";
+    $query = "UPDATE Admin SET image = :filename WHERE Auname = :adminUsername";
 
-    $sql=mysqli_query($conn,$query);
-    if($sql){
-      echo "<script>alert('image changed')</script>";
-    }
+    // $sql=mysqli_query($conn,$query);
+    // if($sql){
+    //   echo "<script>alert('image changed')</script>";
+    // }
 
-    // $stmt = $conn->prepare($sql);
-    // $stmt->bindParam(':filename', $filename);
-    // $stmt->bindParam(':adminUsername', $adminUsername);
-    // $stmt->execute();
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':filename', $filename);
+    $stmt->bindParam(':adminUsername', $adminUsername);
+    $stmt->execute();
 
     }
 
@@ -32,9 +32,11 @@
 <div class="left-column">
   <form action="" method="post">
   <div class="profile" onclick="document.getElementById('fileInput').click()">
+  
       <img id="profileImage" src="images/admin.png" alt="Profile Picture">
+
     </div>
-    <h1>HELLO <?php   echo $_SESSION['admin_user'];?></h1>
+    <h1>hi <?php   echo $_SESSION['admin_user'];?></h1>
     <!-- Hidden file input for profile picture upload -->
     <input type="file" id="fileInput" nam="upload" class="upload-input" onchange="updateProfilePicture(event)" style="display: none"> 
   </form>
